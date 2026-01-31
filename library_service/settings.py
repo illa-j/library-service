3b05de89f5ef8a82b05531b5331073e0956ebf59
illa-j <iliacdmn@gmail.com>
2026-02-01 01:06:43 +0200

    feat: add custom User model, migration, and configure JWT authentication

--- library_service/settings.py
+++ library_service/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt",
+    "users"
 ]
 
 MIDDLEWARE = [
@@ -125,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+AUTH_USER_MODEL = "users.User"
 
 
 REST_FRAMEWORK = {
