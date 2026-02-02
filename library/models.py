920213e406ec4afed72195395a6187b81095268d
illa-j <iliacdmn@gmail.com>
2026-02-02 16:59:52 +0200

    feat: add Borrowing and Payment models, update Book model, and rename price to daily_fee

--- library/models.py
+++ library/models.py
@@ -1,4 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from decimal import Decimal
+
+
+USER = get_user_model()
 
 
 class Author(models.Model):
@@ -23,4 +28,71 @@ class Book(models.Model):
         default=CoverChoices.SOFT
     )
     inventory = models.PositiveIntegerField(default=0)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(blank=True, null=True)
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.PROTECT,
+        related_name="borrowings"
+    )
+    user = models.ForeignKey(
+        USER,
+        on_delete=models.CASCADE,
+        related_name="borrowings"
+    )
+    is_active = models.BooleanField(default=True)
+
+
+class Payment(models.Model):
+    class StatusChoices(models.TextChoices):
+        PAID = "paid", "Paid"
+        PENDING = "pending", "Pending"
+
+    class TypeChoices(models.TextChoices):
+        PAYMENT = "payment", "Payment"
+        FINE = "fine", "Fine"
+
+    status = models.CharField(
+        max_length=10,
+        choices=StatusChoices.choices,
+        default=StatusChoices.PENDING
+    )
+    type = models.CharField(
+        max_length=10,
+        choices=TypeChoices.choices,
+        default=TypeChoices.PAYMENT
+    )
+    borrowing = models.ForeignKey(
+        Borrowing,
+        on_delete=models.PROTECT,
+        related_name="payments"
+    )
+    amount_paid = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+
+    @property
+    def money_to_pay(self) -> Decimal:
+        borrowing = self.borrowing
+
+        borrow_date = borrowing.borrow_date
+        actual_return_date = borrowing.actual_return_date
+        expected_return_date = borrowing.expected_return_date
+        daily_fee = borrowing.book.daily_fee
+
+        total_days = max((actual_return_date - borrow_date).days, 1)
+
+        overdue_days = max((actual_return_date - expected_return_date).days, 0)
+
+        base_amount = Decimal(total_days) * daily_fee
+        overdue_amount = Decimal(overdue_days) * daily_fee * Decimal("1.5")
+
+        return base_amount + overdue_amount
