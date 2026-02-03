070f1988763f576617aeadbc810f2c88799cabb0
illa-j <iliacdmn@gmail.com>
2026-02-03 16:15:35 +0200

    feat: add created_at timestamp to Book model and create corresponding migration

--- library/models.py
+++ library/models.py
@@ -91,6 +91,8 @@ class Book(models.Model):
     inventory = models.PositiveIntegerField(default=0)
     daily_fee = models.DecimalField(max_digits=3, decimal_places=2)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return f"{self.title} by {self.author}"
 
