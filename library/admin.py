ba3cd0a97b98e993b3399cfd7c553b8d23a9799e
illa-j <iliacdmn@gmail.com>
2026-02-08 16:03:04 +0200

    feat: register models in admin site for library and users apps

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from library.models import Author, Book, Borrowing, Payment
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Borrowing)
+admin.site.register(Payment)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth import get_user_model
+
+from users.models import EmailVerificationToken, PasswordChangeToken, TelegramToken
+
+admin.site.register(get_user_model())
+admin.site.register(EmailVerificationToken)
+admin.site.register(PasswordChangeToken)
+admin.site.register(TelegramToken)
